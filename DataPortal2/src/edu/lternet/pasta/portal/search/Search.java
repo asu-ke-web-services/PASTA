2f1bd3f85199e41734c1a150974941d387f2284f
Walter McConnell <Walter.McConnell@asu.edu>
2019-04-29 12:18:46 -0700

    Implement Filtered Simple Search
    Implemented a filter on the simple search functionality. It's not particularly complicated:
    
    * Establish a default filter string that restricts results to a particular organization.
    * Take that string and add it to the  SOLR query as a 'filter query', which filters out all results that don't match. This is the same approach already taken when the portal wants to exclude

--- DataPortal2/src/edu/lternet/pasta/portal/search/Search.java
+++ DataPortal2/src/edu/lternet/pasta/portal/search/Search.java
@@ -6,88 +6,83 @@ import java.util.List;
 import org.apache.solr.client.solrj.util.ClientUtils;
 
 /**
- * The Search class contains methods that are common to all search classes,
- * such as SimpleSearch and AdvancedSearch.
- * 
+ * The Search class contains methods that are common to all search classes, such
+ * as SimpleSearch and AdvancedSearch.
+ *
  * @author dcosta
  *
  */
 public class Search {
-	
+
 	/*
 	 * Class variables
 	 */
-	
-	  protected final static String DEFAULT_DEFTYPE = "edismax";
-	  protected final static String DEFAULT_Q_STRING = "*:*";
-	  protected final static String ECOTRENDS_FILTER = "-scope:ecotrends";
-	  protected final static String LANDSAT_FILTER = "-scope:lter-landsat*";
-	  protected final static String DEFAULT_FIELDS = "id,packageid,title,author,organization,pubdate,coordinates";
-	  public final static int DEFAULT_START = 0;
-	  public final static int DEFAULT_ROWS = 10;
-	  protected final static String DEFAULT_DEBUG = "false";
-	  
-	  // Sort values
-	  public final static String CREATORS_SORT = "responsibleParties";
-	  public final static String PACKAGEID_SORT = "packageid";
-	  public final static String PUBDATE_SORT = "pubdate";
-	  public final static String SCORE_SORT = "score";
-	  public final static String TITLE_SORT = "titles";
-	  public final static String SORT_ORDER_ASC = "asc";
-	  public final static String SORT_ORDER_DESC = "desc";
-	  public final static String DEFAULT_SORT_ORDER = SORT_ORDER_DESC;
-	  public final static String DEFAULT_SORT = String.format("%s,%s", SCORE_SORT, SORT_ORDER_DESC);
-	  
-	  
-	  /*
-	   * Instance variables
-	   */
-	  
-	  protected TermsList termsList;
-	  
-	  
-	  /*
-	   * Constructors
-	   */
-	  
-	  public Search () {
-		  this.termsList = new TermsList();
-	  }
-	  
-	  
+
+	protected final static String DEFAULT_DEFTYPE = "edismax";
+	protected final static String DEFAULT_Q_STRING = "*:*";
+	protected final static String ECOTRENDS_FILTER = "-scope:ecotrends";
+	protected final static String LANDSAT_FILTER = "-scope:lter-landsat*";
+	protected final static String GIOS_FILTER = "organization:(\"CAPLTER\") OR organization:(\"CAP LTER\")";
+	protected final static String DEFAULT_FIELDS = "id,packageid,title,author,organization,pubdate,coordinates";
+	public final static int DEFAULT_START = 0;
+	public final static int DEFAULT_ROWS = 10;
+	protected final static String DEFAULT_DEBUG = "false";
+
+	// Sort values
+	public final static String CREATORS_SORT = "responsibleParties";
+	public final static String PACKAGEID_SORT = "packageid";
+	public final static String PUBDATE_SORT = "pubdate";
+	public final static String SCORE_SORT = "score";
+	public final static String TITLE_SORT = "titles";
+	public final static String SORT_ORDER_ASC = "asc";
+	public final static String SORT_ORDER_DESC = "desc";
+	public final static String DEFAULT_SORT_ORDER = SORT_ORDER_DESC;
+	public final static String DEFAULT_SORT = String.format("%s,%s", SCORE_SORT, SORT_ORDER_DESC);
+
+	/*
+	 * Instance variables
+	 */
+
+	protected TermsList termsList;
+
+	/*
+	 * Constructors
+	 */
+
+	public Search() {
+		this.termsList = new TermsList();
+	}
+
 	/*
 	 * Class methods
 	 */
-	  
-	  /**
-	   * Calls the ClientUtils.escapeQueryChars method but does some
-	   * post-processing by removing escaped spaces and double-quotes. 
-	   * This helps support phrase queries such as "coral reef".
-	   * 
-	   * @param queryString   The query string
-	   * @return escapedString, The escaped query string
-	   */
-	  public static String escapeQueryChars(String queryString) {
-		  String escapedString = ClientUtils.escapeQueryChars(queryString);
-		  if (escapedString != null) {
-			  escapedString = escapedString.replace("\\ ", " "); // don't escape spaces
-			  escapedString = escapedString.replace("\\\"", "\""); // don't escape double quotes
-			  escapedString = escapedString.replace("\\(", "("); // don't escape parens
-			  escapedString = escapedString.replace("\\)", ")"); // don't escape parens
-		  }
-		  return escapedString;
-	  }
-	  
+
+	/**
+	 * Calls the ClientUtils.escapeQueryChars method but does some post-processing
+	 * by removing escaped spaces and double-quotes. This helps support phrase
+	 * queries such as "coral reef".
+	 *
+	 * @param queryString The query string
+	 * @return escapedString, The escaped query string
+	 */
+	public static String escapeQueryChars(String queryString) {
+		String escapedString = ClientUtils.escapeQueryChars(queryString);
+		if (escapedString != null) {
+			escapedString = escapedString.replace("\\ ", " "); // don't escape spaces
+			escapedString = escapedString.replace("\\\"", "\""); // don't escape double quotes
+			escapedString = escapedString.replace("\\(", "("); // don't escape parens
+			escapedString = escapedString.replace("\\)", ")"); // don't escape parens
+		}
+		return escapedString;
+	}
 
 	/**
 	 * Adds a string to a list of terms. An auxiliary method to the
 	 * parseTermsAdvanced() method.
-	 * 
-	 * @param terms
-	 *            list of term strings.
-	 * @param term
-	 *            the new string to add to the list, but only if it isn't an
-	 *            empty string.
+	 *
+	 * @param terms list of term strings.
+	 * @param term  the new string to add to the list, but only if it isn't an empty
+	 *              string.
 	 */
 	private static void addTerm(List<String> terms, final String term) {
 		if (term != null) {
@@ -98,15 +93,13 @@ public class Search {
 			}
 		}
 	}
-	  
-	  
+
 	/**
-	 * Parses search terms from a string. Double-quoted strings that contain
-	 * spaces are parsed as a single term.
-	 * 
-	 * @param value
-	 *            The string value as entered by the user.
-	 * 
+	 * Parses search terms from a string. Double-quoted strings that contain spaces
+	 * are parsed as a single term.
+	 *
+	 * @param value The string value as entered by the user.
+	 *
 	 * @return terms An ArrayList of String objects. Each string is a term.
 	 */
 	protected static List<String> parseTerms(String value) {
@@ -123,8 +116,9 @@ public class Search {
 			c = value.charAt(i);
 
 			if (c == '\"') {
-				/* Termination of a quote-enclosed term. Add the current term to
-				 * list and start a new term.
+				/*
+				 * Termination of a quote-enclosed term. Add the current term to list and start
+				 * a new term.
 				 */
 				if (keepWhitespace) {
 					String phraseTerm = String.format("\"%s\"", currentTerm.toString());
@@ -133,23 +127,20 @@ public class Search {
 				}
 
 				keepWhitespace = !(keepWhitespace); // Toggle keepWhitespace
-			}
-			else
-				if (Character.isWhitespace(c)) {
-					// If we are inside a quote-enclosed term, append the whitespace char.
-					if (keepWhitespace) {
-						currentTerm.append(c);
-					}
-					// Else, add the current term to the list and start a new term.
-					else {
-						addTerm(terms, currentTerm.toString());
-						currentTerm = new StringBuffer();
-					}
-				}
-				else {
-					// Append any non-quote, non-space characters to the current term.
+			} else if (Character.isWhitespace(c)) {
+				// If we are inside a quote-enclosed term, append the whitespace char.
+				if (keepWhitespace) {
 					currentTerm.append(c);
 				}
+				// Else, add the current term to the list and start a new term.
+				else {
+					addTerm(terms, currentTerm.toString());
+					currentTerm = new StringBuffer();
+				}
+			} else {
+				// Append any non-quote, non-space characters to the current term.
+				currentTerm.append(c);
+			}
 		}
 
 		// Add the final term to the list.
@@ -157,32 +148,30 @@ public class Search {
 
 		return terms;
 	}
-	
-	
+
 	/*
 	 * Instance methods
 	 */
-	
+
 	/**
 	 * Accessor method for termsList
-	 * 
+	 *
 	 * @return termsList
 	 */
 	public TermsList getTermsList() {
 		return termsList;
 	}
 
-	
 	/**
 	 * Wraps parens around a query value if it needs them.
-	 * 
+	 *
 	 * For example, the two Solr queries below have different meanings:
-	 * 
-	 * author:jones bug     matches authors named "jones" or text containing "bug"
-	 *                      because the second term is matched against the default
-	 *                      field (df), which in our case is the text field
-	 * author:(jones bug)   matches authors named "jones" or authors named "bug"
-	 * 
+	 *
+	 * author:jones bug matches authors named "jones" or text containing "bug"
+	 * because the second term is matched against the default field (df), which in
+	 * our case is the text field author:(jones bug) matches authors named "jones"
+	 * or authors named "bug"
+	 *
 	 * @return termsList
 	 */
 	public String parenthesizeQueryValue(String value) {
